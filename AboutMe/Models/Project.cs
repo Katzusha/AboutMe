544296c607064d66282f00678977a3f9308e07c8
Andraž Košak <kosak.andraz@gmail.com>
2025-02-15 09:24:01 +0100

    Added database

---
+++ AboutMe/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿namespace AboutMe.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public string Url { get; set; }
+
+        public Project(int _Id, string _Title, string _Description, string _Img, string _Url)
+        {
+            Id = _Id;
+            Title = _Title;
+            Description = _Description;
+            Img = _Img;
+            Url = _Url;
+        }
+    }
+}
